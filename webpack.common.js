d2561c8c0d4557b6f181f835b75f697d54c99983
frxnker <fraanlpzz17@gmail.com>
2026-05-08 12:44:21 +0200

    Estructura

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/js/app.js',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
     filename: 'js/app.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
